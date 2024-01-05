c1a000ad655abd0cdbfc04525d3cc097fa9f87f0
Elliott Tuan <gbs.tuan@gmail.com>
2024-01-06 03:12:28 +0800

    Add product details page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,60 @@
-import React from "react";
+import prisma from "@/lib/db/prisma";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import PriceTag from "@/components/PriceTag";
+import { cache } from "react";
+import { Metadata } from "next";
 
-export default function page() {
-  return <div>Products ID</div>;
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (!product) notFound();
+  return product;
+});
+
+export async function generateMetadata({
+  params: { id },
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProduct(id);
+
+  return {
+    title: product.name + " - Mood",
+    description: product.description,
+    openGraph: {
+      images: [
+        {
+          url: product.imageUrl,
+        },
+      ],
+    },
+  };
+}
+
+export default async function ProductPage({
+  params: { id },
+}: ProductPageProps) {
+  const product = await getProduct(id);
+
+  return (
+    <div>
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={500}
+        height={500}
+        className="rounded-lg"
+        priority
+      />
+      <div>
+        <h1 className="text-5xl font-bold">{product.name}</h1>
+        <PriceTag price={product.price} className="mt-4" />
+        <p className="py-6">{product.description}</p>
+      </div>
+    </div>
+  );
 }
